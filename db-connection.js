45ca0738c281ba1417cda44c4a6819e12950477d
marcos8896 <elyomarcos@gmail.com>
2018-03-17 18:37:37 -0600

    Initial commit

---
+++ db-connection.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    multipleStatements: process.env.MULTI_STATEMENTS
+    
+});
+
+conn.connect( err => {
+    if (err) {
+        console.log('Error trying to connect with database: ' + err.stack);
+        return;
+    }
+});
+
+module.exports =  conn;
